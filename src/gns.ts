05e691186996868dd6984b6b4f09d1580ca513a9
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2023-11-26 22:43:46 -0500

    Revised GNS.ts

--- src/gns.ts
+++ src/gns.ts
@@ -3,10 +3,11 @@ import {
   TokensAdded as TokensAddedEvent,
   TokensPulled as TokensPulledEvent,
   TokensRemoved as TokensRemovedEvent,
+  // Import other events if needed
 } from '../generated/Billing/Billing'
 import { Account as AccountEntity } from '../generated/schema'
 
-// Handlers for the Billing Contract
+// Handle the TokensAdded event
 export function handleTokensAdded(event: TokensAddedEvent): void {
   let account = AccountEntity.load(event.params.user.toHex())
   if (!account) {
@@ -17,6 +18,7 @@ export function handleTokensAdded(event: TokensAddedEvent): void {
   account.save()
 }
 
+// Handle the TokensPulled event
 export function handleTokensPulled(event: TokensPulledEvent): void {
   let account = AccountEntity.load(event.params.user.toHex())
   if (account) {
@@ -25,32 +27,11 @@ export function handleTokensPulled(event: TokensPulledEvent): void {
   }
 }
 
+// Handle the TokensRemoved event
 export function handleTokensRemoved(event: TokensRemovedEvent): void {
   let account = AccountEntity.load(event.params.from.toHex())
   if (account) {
     account.billingBalance = account.billingBalance.minus(event.params.amount)
     account.save()
   }
-}
-
-// Template Handlers for the GNS Contract
-// Replace with the actual logic based on your GNS contract's ABI
-export function handleSubgraphPublished(event: SubgraphPublishedEvent): void {
-  // Implementation logic
-}
-
-export function handleSubgraphUpgraded(event: SubgraphUpgradedEvent): void {
-  // Implementation logic
-}
-
-export function handleSubgraphVersionUpdated(
-  event: SubgraphVersionUpdatedEvent,
-): void {
-  // Implementation logic
-}
-
-export function handleSubgraphReceivedFromL1(
-  event: SubgraphReceivedFromL1Event,
-): void {
-  // Implementation logic
 }
