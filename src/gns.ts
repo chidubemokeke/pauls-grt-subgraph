09f0a6b016ed079c54efed6d17ce63c4de8b16ab
chidubemokeke <92036409+chidubemokeke@users.noreply.github.com>
2023-12-11 01:19:11 +0100

    Fixed errors

--- src/gns.ts
+++ src/gns.ts
@@ -1,59 +1,58 @@
-/*import { BigInt } from '@graphprotocol/graph-ts'
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   TokensAdded as TokensAddedEvent,
   TokensPulled as TokensPulledEvent,
   TokensRemoved as TokensRemovedEvent,
   // Import other events if needed
-} from '../generated/Billing/Billing'
-import { Account as AccountEntity } from '../generated/schema'
+} from "../generated/Billing/Billing";
+import { Account as AccountEntity } from "../generated/schema";
 
 // Handle the TokensAdded event
 export function handleTokensAdded(event: TokensAddedEvent): void {
-  let account = AccountEntity.load(event.params.user.toHex())
+  let account = AccountEntity.load(event.params.user.toHex());
 
   if (!account) {
-    account = new AccountEntity(event.params.user.toHex())
-    account.billingBalance = BigInt.fromI32(0)
-    account.queryFeesPaid = BigInt.fromI32(0)
+    account = new AccountEntity(event.params.user.toHex());
+    account.billingBalance = BigInt.fromI32(0);
+    account.queryFeesPaid = BigInt.fromI32(0);
   }
 
   // Use a different approach to check if queryFeesPaid is null
   if (!account.queryFeesPaid) {
-    account.queryFeesPaid = BigInt.fromI32(0)
+    account.queryFeesPaid = BigInt.fromI32(0);
   }
 
-  account.billingBalance = account.billingBalance.plus(event.params.amount)
-  account.queryFeesPaid = account.queryFeesPaid.plus(event.params.amount)
-  account.save()
+  account.billingBalance = account.billingBalance.plus(event.params.amount);
+  account.queryFeesPaid = account.queryFeesPaid.plus(event.params.amount);
+  account.save();
 }
 
 // Handle the TokensPulled event
 export function handleTokensPulled(event: TokensPulledEvent): void {
-  let account = AccountEntity.load(event.params.user.toHex())
+  let account = AccountEntity.load(event.params.user.toHex());
   if (!account) {
-    account = new AccountEntity(event.params.user.toHex())
-    account.billingBalance = BigInt.fromI32(0)
-    account.queryFeesPaid = BigInt.fromI32(0)
+    account = new AccountEntity(event.params.user.toHex());
+    account.billingBalance = BigInt.fromI32(0);
+    account.queryFeesPaid = BigInt.fromI32(0);
   }
-  account.billingBalance = account.billingBalance.minus(event.params.amount)
-  account.queryFeesPaid = account.queryFeesPaid.minus(event.params.amount)
-  account.save()
+  account.billingBalance = account.billingBalance.minus(event.params.amount);
+  account.queryFeesPaid = account.queryFeesPaid.minus(event.params.amount);
+  account.save();
 }
 
 // Handle the TokensRemoved event
 export function handleTokensRemoved(event: TokensRemovedEvent): void {
-  let account = AccountEntity.load(event.params.from.toHex())
+  let account = AccountEntity.load(event.params.from.toHex());
   if (!account) {
-    account = new AccountEntity(event.params.from.toHex())
-    account.billingBalance = BigInt.fromI32(0)
-    account.queryFeesPaid = BigInt.fromI32(0)
+    account = new AccountEntity(event.params.from.toHex());
+    account.billingBalance = BigInt.fromI32(0);
+    account.queryFeesPaid = BigInt.fromI32(0);
   }
-  account.billingBalance = account.billingBalance.minus(event.params.amount)
-  account.queryFeesPaid = account.queryFeesPaid.minus(event.params.amount)
-  account.save()
-}*/
+  account.billingBalance = account.billingBalance.minus(event.params.amount);
+  account.queryFeesPaid = account.queryFeesPaid.minus(event.params.amount);
+  account.save();
+}
 
-import { BigInt } from "@graphprotocol/graph-ts";
 import {
   SubgraphPublished as SubgraphPublishedEvent,
   SubgraphUpgraded as SubgraphUpgradedEvent,
